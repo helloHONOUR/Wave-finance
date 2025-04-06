6b5f3adb4996391d7054eda64af2c06041ce14e1
alajemba-vik <alajemba.dev@gmail.com>
2025-04-06 13:17:17 +0000

    just another push

--- lib/main.dart
+++ lib/main.dart
@@ -29,7 +29,7 @@ class MyApp extends StatelessWidget {
           }
 
           if (snapshot.data == null) {
-            return SignupScreen();
+            return InputPhoneNum();
           }
           if (snapshot.data != null) {
             print('not null');
